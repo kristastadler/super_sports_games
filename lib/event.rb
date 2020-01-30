254636c917ce6f6cd3da0683eceda8178833ca25
“Krista <stadler.krista@gmail..com>
2020-01-30 15:40:37 -0700

    working on average age

--- lib/event.rb
+++ lib/event.rb
@@ -1,9 +1,11 @@
 class Event
-  attr_reader :name, :ages
+  attr_reader :name, :float_ages
+  attr_accessor :ages
 
   def initialize(name_parameter, ages_parameter)
     @name = name_parameter
     @ages = ages_parameter
+    @float_age = []
   end
 
   def max_age
@@ -13,7 +15,16 @@ class Event
   def min_age
     ages.min
   end
-     
+
+
+
+  def average_age
+    ages.each do |age|
+      puts @float_age << age.to_f
+    end
+    ages.sum / ages.count
+  end
+
 end
 
 
